51a1600f469cd688a28cdcde9a485c7e4510bcf9
Oliver Alcaraz <s242289@students.aie.edu.au>
2025-01-31 16:06:03 +1100

    Created demo hello world project with visual studio .gitignore

---
+++ Intro Project/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int numberOfOdds = 15;
+    int total = numberOfOdds * numberOfOdds;
+
+    std::cout << "Total of the first " << numberOfOdds << " odd numbers is " << total << "\n";
+
+    return 0;
+}
